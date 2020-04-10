d55a9975425e21d3f08a16c554ac28f2b90e4c9a
gcmartin <gscalica@gmail.com>
2020-04-09 21:47:49 -0700

    Add questions to questions array and include inquirer to ask questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,36 @@
-const questions = [
+// requiring inquirer and fs
+const inquirer = require("inquirer");
 
+// questions for inquirer to ask
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message:  "What is your GitHub username?"
+    }, 
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message:  "Please provide a brief description of your project."
+    },
 ];
+
+
+// inquirer asks questions
+inquirer.prompt(questions).then(function(answers){
+    console.log(answers);
+})
+
+
+
+
+
+// =========================================
 
 function writeToFile(fileName, data) {
 }
